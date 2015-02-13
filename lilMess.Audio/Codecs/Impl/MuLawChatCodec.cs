e52369245344d7cf669c671c97f4cb4700cef429
AlexTGM <alexandrmineev@gmail.com>
2015-02-13 23:20:14 +0300

    Оеализация MuLawCodec

--- lilMess.Audio/Codecs/Impl/MuLawChatCodec.cs
+++ lilMess.Audio/Codecs/Impl/MuLawChatCodec.cs
@@ -2,10 +2,12 @@
 {
     using System;
 
-    using NAudio.Codecs;
+    using lilMess.Audio.Decoders.Impl;
+    using lilMess.Audio.Encoders.Impl;
+
     using NAudio.Wave;
 
-    public class MuLawChatCodec : INetworkChatCodec
+    internal class MuLawChatCodec : INetworkChatCodec
     {
         public string Name { get { return "G.711 mu-law"; } }
 
@@ -13,29 +15,31 @@
 
         public WaveFormat RecordFormat { get { return new WaveFormat(8000, 16, 1); } }
 
-        public bool IsAvailable { get { return true; } }
-
         public byte[] Encode(byte[] data, int offset, int length)
         {
             var encoded = new byte[length / 2];
-            int outIndex = 0;
-            for (int n = 0; n < length; n += 2)
+            var outIndex = 0;
+
+            for (var n = 0; n < length; n += 2)
             {
-                encoded[outIndex++] = MuLawEncoder.LinearToMuLawSample(BitConverter.ToInt16(data, offset + n));
+                encoded[outIndex++] = MuLawEncoder.Encode(BitConverter.ToInt16(data, offset + n));
             }
+
             return encoded;
         }
 
         public byte[] Decode(byte[] data, int offset, int length)
         {
             var decoded = new byte[length * 2];
-            int outIndex = 0;
-            for (int n = 0; n < length; n++)
+            var outIndex = 0;
+
+            for (var n = 0; n < length; n++)
             {
-                short decodedSample = MuLawDecoder.MuLawToLinearSample(data[n + offset]);
+                var decodedSample = MuLawDecoder.Decode(data[n + offset]);
                 decoded[outIndex++] = (byte)(decodedSample & 0xFF);
                 decoded[outIndex++] = (byte)(decodedSample >> 8);
             }
+
             return decoded;
         }
 
---
+++ lilMess.Audio/Encoders/Impl/MuLawEncoder.cs
@@ -0,0 +1,41 @@
+﻿namespace lilMess.Audio.Encoders.Impl
+{
+    public class MuLawEncoder
+    {
+        private const int cBias = 0x84;
+
+        private const int cClip = 32635;
+
+        private static readonly byte[] MuLawCompressTable = new byte[256]
+                                                                {
+                                                                    0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4,
+                                                                    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5,
+                                                                    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                                                                    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6,
+                                                                    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                                                                    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                                                                    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+                                                                    6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+                                                                    7, 7, 7, 7
+                                                                };
+
+        public static byte Encode(short sample)
+        {
+            var sign = (sample >> 8) & 0x80;
+            if (sign != 0) sample = (short)-sample;
+            if (sample > cClip) sample = cClip;
+            sample = (short)(sample + cBias);
+            var exponent = (int)MuLawCompressTable[(sample >> 7) & 0xFF];
+            var mantissa = (sample >> (exponent + 3)) & 0x0F;
+            var compressedByte = ~(sign | (exponent << 4) | mantissa);
+
+            return (byte)compressedByte;
+        }
+    }
+}
